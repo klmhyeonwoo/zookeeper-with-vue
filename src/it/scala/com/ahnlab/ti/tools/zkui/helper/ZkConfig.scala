6086c69b5eefb1787ab4163588397461b25f61eb
jowonhyeong <wonhyeong.jo@ahnlab.com>
2024-03-23 12:19:54 +0900

    [수정사항] 통합 테스트를 위한 host, ip 수정

--- src/it/scala/com/ahnlab/ti/tools/zkui/helper/ZkConfig.scala
+++ src/it/scala/com/ahnlab/ti/tools/zkui/helper/ZkConfig.scala
@@ -1,11 +1,11 @@
 package com.ahnlab.ti.tools.zkui.helper
 
 object ZkConfig {
-  //private val DaZkHosts: String = "da-zk01.atip.ahnlab.com:2181,da-zk02.atip.ahnlab.com:2181,da-zk03.atip.ahnlab.com:2181"
-  val DaZkHosts: String = "localhost:2184"
+  private val DaZkHosts: String = "da-zk01.atip.ahnlab.com:2181,da-zk02.atip.ahnlab.com:2181,da-zk03.atip.ahnlab.com:2181"
+  //val DaZkHosts: String = "localhost:2184"
   val ClusterName: String = "asd-app"
-  val ServerHost: String = "localhost"
-  //val ServerHost: String = "app02.asd.ahnlab.com"
-  val ServerIp: Int = 8080
-  //val ServerIp: Int = 20001
+  //val ServerHost: String = "localhost"
+  val ServerHost: String = "app02.asd.ahnlab.com"
+  //val ServerIp: Int = 8080
+  val ServerIp: Int = 12006
 }
