62b2d7db8f19787e57e69454b3d294b7ed84c255
jowonhyeong <wonhyeong.jo@ahnlab.com>
2024-03-23 12:22:07 +0900

    [수정사항] 코드 수정

--- src/it/scala/com/ahnlab/ti/tools/zkui/helper/ZkConfig.scala
+++ src/it/scala/com/ahnlab/ti/tools/zkui/helper/ZkConfig.scala
@@ -1,7 +1,7 @@
 package com.ahnlab.ti.tools.zkui.helper
 
 object ZkConfig {
-  private val DaZkHosts: String = "da-zk01.atip.ahnlab.com:2181,da-zk02.atip.ahnlab.com:2181,da-zk03.atip.ahnlab.com:2181"
+  val DaZkHosts: String = "da-zk01.atip.ahnlab.com:2181,da-zk02.atip.ahnlab.com:2181,da-zk03.atip.ahnlab.com:2181"
   //val DaZkHosts: String = "localhost:2184"
   val ClusterName: String = "asd-app"
   //val ServerHost: String = "localhost"
