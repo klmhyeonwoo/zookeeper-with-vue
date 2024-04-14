7db29047f4cb5241bdec7db9ffc0b28ea56c1ea3
klmhyeonwoo <gentlemonster77@likelion.org>
2024-04-15 01:22:22 +0900

    fix: 경고 모달 레이아웃 일부 수정

--- src/components/common/modal/WarningModal.vue
+++ src/components/common/modal/WarningModal.vue
@@ -12,9 +12,36 @@ console.log(modalInfo.value);
 
 <template>
   <ModalWrapper>
-    <div> 중복된 값이 존재합니다 </div>
-    <div> {{ modalInfo.clusterAddNodeAddress }} </div>
-    <div> 현재 노드의 값 : {{ modalInfo.clusterCurNodeValue }}</div>
-    <div> 변경될 노드 : {{ modalInfo.clusterAddNodeValue }}</div>
+    <div :class="$style.title"> 등록하고자 하는 노드(
+      <span :class="$style.address"> {{ modalInfo.clusterAddNodeAddress }} </span>
+      )가 존재합니다 </div>
+    <div :class="$style.values">
+      <div> {{ modalInfo.clusterCurNodeValue }} (현재)</div>
+      <span> >>> </span>
+      <div> {{ modalInfo.clusterAddNodeValue }} (변경)</div>
+    </div>
   </ModalWrapper>
 </template>
+
+<style module>
+  .title {
+    font-size: 1.3rem;
+    font-weight: 600;
+    color: #212529;
+  }
+
+  .address {
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+
+  .values {
+    display: flex;
+    column-gap: 1rem;
+  }
+
+  .values > div {
+    padding: 1rem;
+    border-radius: 0.4rem;
+  }
+</style>
